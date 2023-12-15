c5e992666720b1669f7c0e746c8213d5ee5e190d
Saud <65331551+saudsami@users.noreply.github.com>
2023-12-15 09:47:56 +0500

    updates

--- flutter-reference-app/lib/stream-raw-video-and-audio/agora_manager_raw_video_audio.dart
+++ flutter-reference-app/lib/stream-raw-video-and-audio/agora_manager_raw_video_audio.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
+
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_reference_app/authentication-workflow/agora_manager_authentication.dart';
 import 'package:flutter_reference_app/agora-manager/agora_manager.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class AgoraManagerRawVideoAudio extends AgoraManagerAuthentication {
+  late File _playbackAudioFile;
+  late File _audioFile;
+  late AudioFrameObserver audioFrameObserver;
+  late VideoFrameObserver videoFrameObserver;
+
   AgoraManagerRawVideoAudio({
     required ProductName currentProduct,
     required Function(String message) messageCallback,
@@ -34,29 +43,15 @@ class AgoraManagerRawVideoAudio extends AgoraManagerAuthentication {
     return manager;
   }
 
-  AudioFrameObserver audioFrameObserver = AudioFrameObserver(
-      onRecordAudioFrame: (String channelId, AudioFrame audioFrame) {
-        // Gets the captured audio frame
-      },
-      onPlaybackAudioFrame: (String channelId, AudioFrame audioFrame) {
-        // Gets the audio frame for playback
-        debugPrint('[onPlaybackAudioFrame] audioFrame: ${audioFrame.toJson()}');
-      }
-  );
-
-  VideoFrameObserver videoFrameObserver = VideoFrameObserver(
-      onCaptureVideoFrame: (VideoSourceType videoSourceType, VideoFrame videoFrame) {
-        // The video data that this callback gets has not been pre-processed
-        // After pre-processing, you can send the processed video data back
-        // to the SDK through this callback
-        debugPrint('[onCaptureVideoFrame] videoFrame: ${videoFrame.toJson()}');
-      },
-      onRenderVideoFrame: (String channelId, int remoteUid, VideoFrame videoFrame) {
-        // Occurs each time the SDK receives a video frame sent by the remote user.
-        // In this callback, you can get the video data before encoding.
-        // You then process the data according to your particular scenario.
-      }
-  );
+  Future<void> write(AudioFrame audioFrame) async {
+    if (!isJoined) {
+      return;
+    }
+    if (audioFrame.buffer != null) {
+      await _audioFile.writeAsBytes(audioFrame.buffer!.toList(),
+          mode: FileMode.append, flush: true);
+    }
+  }
 
   @override
   Future<void> setupAgoraEngine() async {
@@ -95,8 +90,75 @@ class AgoraManagerRawVideoAudio extends AgoraManagerAuthentication {
         channel: sampleNumOfChannels,
         samplesPerCall: samplesPerCall);
 
+
+    audioFrameObserver =  AudioFrameObserver(
+        onRecordAudioFrame: (String channelId, AudioFrame audioFrame) async {
+          // Gets the captured audio frame
+          await write(audioFrame);
+        },
+        onPlaybackAudioFrame: (String channelId, AudioFrame audioFrame) {
+          // Gets the audio frame for playback
+          debugPrint('[onPlaybackAudioFrame] audioFrame: ${audioFrame.toJson()}');
+        }
+    );
+
     agoraEngine!.getMediaEngine().registerAudioFrameObserver(audioFrameObserver);
+
+    videoFrameObserver = VideoFrameObserver(
+        onCaptureVideoFrame: (VideoSourceType videoSourceType, VideoFrame videoFrame) {
+          // The video data that this callback gets has not been pre-processed
+          // After pre-processing, you can send the processed video data back
+          // to the SDK through this callback
+          debugPrint('[onCaptureVideoFrame] videoFrame: ${videoFrame.toJson()}');
+        },
+        onRenderVideoFrame: (String channelId, int remoteUid, VideoFrame videoFrame) {
+          // Occurs each time the SDK receives a video frame sent by the remote user.
+          // In this callback, you can get the video data before encoding.
+          // You then process the data according to your particular scenario.
+        }
+    );
+
     agoraEngine!.getMediaEngine().registerVideoFrameObserver(videoFrameObserver);
+  }
+
+  Future<void> startAudioFrameRecord() async {
+    Directory appDocDir = Platform.isAndroid
+        ? (await getExternalStorageDirectory())!
+        : await getApplicationDocumentsDirectory();
+
+    _audioFile = File(path.join(appDocDir.absolute.path, 'record_audio.raw'));
+    if (await _audioFile.exists()) {
+      await _audioFile.delete();
+    }
+    await _audioFile.create();
+    messageCallback('onRecordAudioFrame file output to: ${_audioFile.absolute.path}');
+
+    _playbackAudioFile = File(path.join(
+      appDocDir.absolute.path,
+      'playback_audio.raw',
+    ));
+    if (await _playbackAudioFile.exists()) {
+      await _playbackAudioFile.delete();
+    }
+    await _playbackAudioFile.create();
+    messageCallback(
+        'onPlaybackAudioFrame file output to: ${_playbackAudioFile.absolute.path}');
+
+    agoraEngine!.getMediaEngine().registerAudioFrameObserver(audioFrameObserver);
+    await agoraEngine!.setPlaybackAudioFrameParameters(
+        sampleRate: 32000,
+        channel: 1,
+        mode: RawAudioFrameOpModeType.rawAudioFrameOpModeReadOnly,
+        samplesPerCall: 1024);
+    await agoraEngine!.setRecordingAudioFrameParameters(
+        sampleRate: 32000,
+        channel: 1,
+        mode: RawAudioFrameOpModeType.rawAudioFrameOpModeReadOnly,
+        samplesPerCall: 1024);
+  }
+
+  void stopAudioFrameRecord() {
+    agoraEngine!.getMediaEngine().unregisterAudioFrameObserver(audioFrameObserver);
   }
 
   @override
